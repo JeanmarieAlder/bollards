eb0e752e44c9493c5f4b39d472666f13574ab698
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2021-09-01 13:49:21 +0200

    improve css for blurry mobile images

--- src/list/BollardCardList.js
+++ src/list/BollardCardList.js
@@ -5,7 +5,7 @@ import './BollardCardList.css';
 
 const BollardCardList = ({bollards}) => {
     return (
-        <div className='p-5 card-container row text-center justify-content-center'>
+        <div className='p-2 card-container row text-center justify-content-center'>
             {
                 bollards.sort((a, b) => a.b_number - b.b_number)
                 .map((bollard, i) => {
