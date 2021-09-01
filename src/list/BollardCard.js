e624a6a11f8614866bcfc00ebb254357b35f9b7d
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2021-09-01 16:24:55 +0200

    revision of css

--- src/list/BollardCard.js
+++ src/list/BollardCard.js
@@ -4,19 +4,22 @@ import './BollardCard.css';
 const BollardCard = ({bollard}) => {
     const [imageLoaded, setImageLoaded] = useState(false);
     return (
-        <div className='card bollard-card'>
-            {
-                imageLoaded ? null :
-                <div className="img-loading-div">
-                    <div className="spinner-grow text-secondary" role="status">
+        <div className='col-6 col-md-3 bollard-card'>
+            <div className='card'>
+                {
+                    imageLoaded ? null :
+                    <div className="img-loading-div">
+                        <div className="spinner-grow text-secondary" role="status">
+                        </div>
                     </div>
+                }
+                <img className='img-preview' alt='bollard' src={`http://localhost:5000/static/img/bollards_icon/${bollard.image_icon}`} onLoad={() => setImageLoaded(true)}/>
+                <div className='card-body bollard-card-text-div'>
+                    <h2 className="card-title">No {bollard.b_number}{bollard.b_letter}</h2>
+                    <p className="card-text"><i>{bollard.b_name === '' ? 'Unnamed' : bollard.b_name}</i></p>
                 </div>
-            }
-            <img className='img-preview' alt='bollard' src={`http://localhost:5000/static/img/bollards_icon/${bollard.image_icon}`} onLoad={() => setImageLoaded(true)}/>
-            <div className='card-body bollard-card-text-div'>
-                <h2 className="card-title">No {bollard.b_number}{bollard.b_letter}</h2>
-                <p className="card-text"><i>{bollard.b_name === '' ? 'Unnamed' : bollard.b_name}</i></p>
             </div>
+            
         </div>
     );
 }
