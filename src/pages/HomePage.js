58a093be78dd66cbe8daca759a4a61adb72ad23a
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2021-10-13 00:08:47 +0200

    added title and link to map on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,14 +3,21 @@ import BollardCardList from "../list/BollardCardList"
 import BollardCardListFiltered from "../list/BollardCardListFiltered"
 import SearchBox from "../list/SearchBox"
 import { fetchDataApi } from '../utils/FetchData';
+import imgMapLink from "../assets/img/map_link.png"
+import "./HomePage.css";
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 
     const [bollardsList, setBollardsList] = useState([]);
     const [searchField, setSearchField] = useState('');
+    const [showTitle, setShowTitle] = useState('fade-in')
 
     useEffect(() => {
         fetchDataApi('bollards/list', setBollardsList);
+        setTimeout(() => {
+            setShowTitle('');
+        }, 100);
     }, [])
 
     const onSearchInput = (event) => {
@@ -36,6 +43,14 @@ const HomePage = () => {
 
     return (
         <div className='home-page-div'>
+            <div id="home-page-title-div">
+                <p id="home-page-title" className={"home-page-title " + showTitle} >bollards.ch</p>
+            </div>
+            <div className="map-link-div">
+                <Link to={`/map`}>
+                    <img className="map-link-image" src={imgMapLink} alt="map-link"></img>
+                </Link>
+            </div>
             <SearchBox onSearchInput={onSearchInput}/>
             {
                 filtered ? <BollardCardListFiltered bollards={filteredBollards} /> : <BollardCardList bollards={bollardsList} />
