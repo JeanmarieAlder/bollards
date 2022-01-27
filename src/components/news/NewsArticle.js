92bb72bb2bc2670faffb6a7bd3a033f5e3ea585b
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2022-01-27 12:42:35 +0100

    add alt prop to img

--- src/components/news/NewsArticle.js
+++ src/components/news/NewsArticle.js
@@ -9,7 +9,7 @@ const NewsArticle = () => {
         <div className='news-article'>
             <div className='news-article-title'><h2>Welcome to bollards.ch news!</h2></div>
             <div className='news-article-infos'>Posted on: November 18, 2021 by Wakxy13</div>
-            <div className='news-article-image-div'><img className='news-article-image' src={articleImageDefault}></img></div>
+            <div className='news-article-image-div'><img className='news-article-image' alt={""} src={articleImageDefault}></img></div>
             <div className='news-article-abstract'><b>This page will cover what's hapenning around this website. Get to know useful insiders and tips to visit the swiss border and its border bollards.</b></div>
             <div className='news-article-content'>Dear visitor, this page will soon be available. Make sure to check out the other pages in the meantime! Go to <Link to={'/list'}>the list of all bollards</Link> or to <Link to='/map'>the interactive map</Link>.</div>
         </div>
